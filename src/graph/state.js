28bd7762ab63ab7e294f925e0909eaa9004bb282
PREETAM SHAH <40589141+preetamshah@users.noreply.github.com>
2025-03-16 21:05:54 -0700

    Update state.js

--- src/graph/state.js
+++ src/graph/state.js
@@ -1,1 +1,11 @@
-
+class AppState {
+  constructor() {
+    this.inputText = null;
+    this.extractedValue = null;
+    this.valueType = null;
+    this.configurations = null;
+    this.isValid = null;
+    this.responseMessage = null;
+  }
+}
+module.exports = AppState;

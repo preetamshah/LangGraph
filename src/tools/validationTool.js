960c58bee5c501f4474b214843f8df807872967e
PREETAM SHAH <40589141+preetamshah@users.noreply.github.com>
2025-03-16 21:09:09 -0700

    Create validationTool.js

---
+++ src/tools/validationTool.js
@@ -0,0 +1,17 @@
+const { z } = require('zod');
+const StructuredTool = require('./customTool');
+const configSchema = z.object({
+  configurations: z.object({ rn: z.string().optional(), vin: z.string().optional(), config: z.string().optional() }).describe('The configuration object to validate'),
+});
+class ValidationTool {
+  constructor() {
+    this.validateConfig = new StructuredTool({
+      name: 'validateConfig',
+      description: 'Validates whether a given configuration object is valid.',
+      schema: configSchema,
+      execute: async ({ configurations }) => { return !!configurations.config; },
+    });
+  }
+  async validateConfig(configurations) { return this.validateConfig.call({ configurations }); }
+}
+module.exports = new ValidationTool();
